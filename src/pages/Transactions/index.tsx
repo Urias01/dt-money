8eda63b2c6ecd15011ceed93d4891ec0422fd928
Urias01 <uriasluis64@gmail.com>
2024-03-06 12:43:57 -0300

    create summary

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,11 @@
 import { Header } from "../../components/Header";
+import { Summary } from "../../components/Sumarry";
 
 export function Transactions() {
   return (
     <div>
       <Header />
+      <Summary />
     </div>
   )
 }
